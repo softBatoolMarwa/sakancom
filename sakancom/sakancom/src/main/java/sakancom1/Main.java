28a72dd0a7e2744038337e064227b940ad0716e2
MarwaBaniFadil <137709477+MarwaBaniFadil@users.noreply.github.com>
2023-08-05 18:13:17 +0300

    Add files via upload

--- sakancom/sakancom/src/main/java/sakancom1/Main.java
+++ sakancom/sakancom/src/main/java/sakancom1/Main.java
@@ -58,10 +58,10 @@ public static  Boolean isUserFound (String userName , String passWord) {
 			
 			if (t.equals(tenantLinst.get(i).getname()) )  {
 				if (tenantLinst.get(i).getFurniture()!=null) {
-					LOGGER.log(Level.INFO,"I have this used furniture for sale at good prices :\n" , tenantLinst.get(i).getFurniture() );
-		
-					
-					
+					String neww=  tenantLinst.get(i).getFurniture()   ;
+					LOGGER.log(Level.INFO,"I have this used furniture for sale at good prices :\n" );
+					LOGGER.log(Level.INFO,neww );
+
 					returnn= true;
 				}
 				else {
@@ -93,15 +93,15 @@ public static boolean addUsedFurniture(String Furniture,String user) {
 		for (int i=0 ; i<tenantLinst.size() ; i++) {
 		if(tenantLinst.get(i).getname().equals(usernamee)) {
 			a=true;
-			LOGGER.log(Level.INFO,"your name : ",tenantLinst.get(i).getname());
-			LOGGER.log(Level.INFO,"your phone : ",tenantLinst.get(i).getphone());
-			LOGGER.log(Level.INFO,"your age : ",tenantLinst.get(i).getage());
-			LOGGER.log(Level.INFO,"your majer : ",tenantLinst.get(i).getmajer());
-			LOGGER.log(Level.INFO,"your furniture : ",tenantLinst.get(i).getFurniture());
+			LOGGER.log(Level.INFO,"your name : "+tenantLinst.get(i).getname());
+			LOGGER.log(Level.INFO,"your phone : "+tenantLinst.get(i).getphone());
+			LOGGER.log(Level.INFO,"your age : "+tenantLinst.get(i).getage());
+			LOGGER.log(Level.INFO,"your majer : "+tenantLinst.get(i).getmajer());
+			LOGGER.log(Level.INFO,"your furniture : "+tenantLinst.get(i).getFurniture());
 			
 			for (int j=0 ; j<houseAndTennantlist.size() ; j++) {
 				if(usernamee.equals(houseAndTennantlist.get(j).getTenants())) {
-					LOGGER.log(Level.INFO,"The apartment number booked in your name is  : ",houseAndTennantlist.get(j).gethousingId());
+					LOGGER.log(Level.INFO,"The apartment number booked in your name is  : "+houseAndTennantlist.get(j).gethousingId());
 					IdHouse=houseAndTennantlist.get(j).gethousingId();
 				
 					
@@ -1014,6 +1014,7 @@ flag=true;
 	
 	public static void main(String args[]) {
 		
+	
 		fullInformation();
 		input22 = new Scanner(System.in);
 		LOGGER.log(Level.INFO,"Enter your username please");
